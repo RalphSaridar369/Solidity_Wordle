a23124eaa011f5809d24254c300cefec8f066748
RalphSaridar55 <ralphsaridar@hotmail.com>
2022-02-24 12:39:57 +0200

    Basic DApp tempalte

--- src/contracts/BasicToken.sol
+++ src/contracts/BasicToken.sol
@@ -1,26 +1,26 @@
-pragma solidity ^0.8.0;
+pragma solidity 0.8.11;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-contract BasicToken is ERC20{
+contract BasicToken is ERC20 {
     
     address private owner;
 
-    constructor() ERC20('BasicToken','BST'){
+    constructor() ERC20("BasicToken", "BST"){
         _mint(msg.sender, 10000);
         owner = msg.sender;
     }
 
-    // modifier onlyOwner {
-    //    require(msg.sender == owner,"You are not the owner");
-    //    _;
-    // }
+    modifier onlyOwner {
+       require(msg.sender == owner,"You are not the owner");
+       _;
+    }
 
-    // function burnTokens(uint256 _amount) public onlyOwner{
-    //     _burn(owner,_amount);
-    // }
+    function burnTokens(uint256 _amount) public onlyOwner(){
+        _burn(owner,_amount);
+    }
 
-    // function mintTokens(uint256 _amount) public onlyOwner{
-    //     _mint(owner,_amount);
-    // }
+    function mintTokens(uint256 _amount) public onlyOwner(){
+        _mint(owner,_amount);
+    }
 }
