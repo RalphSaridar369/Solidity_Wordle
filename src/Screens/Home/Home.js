6991a15c1532869524c6081e367c55ed2021eed0
RalphSaridar55 <ralphsaridar@hotmail.com>
2022-04-04 23:02:15 +0300

    Working on UI, contract and web3 implementation

--- src/Screens/Home/Home.js
+++ src/Screens/Home/Home.js
@@ -8,15 +8,23 @@ const Home = () => {
   
   const [lives,setLives] = useState();
   const [data, setData] = useState([]);
-  const { contract, account, price, owner, startedGame, setStartedGame } = useContext(MainContext)
+  const { contract, account, price, wordsCount, owner, startedGame, setStartedGame } = useContext(MainContext)
 
   useEffect(() => {
-    console.log(startedGame)
+    const getData = async() =>{
+      if(startedGame){
+        let gameData = await contract.methods.games(startedGame).call();
+        console.log(startedGame)
+        console.log(gameData)
+      }
+    }
+    getData();
   }, [])
 
   const startGame =async()=>{
+    let choice = Math.floor(Math.random() * (wordsCount-1)) + 1
     console.log(parseInt(price.fee).toString())
-    await contract.methods.startGame(0).send({from:account, value:parseInt(price.fee).toString()})
+    await contract.methods.startGame(choice).send({from:account, value:parseInt(price.fee).toString()})
     .once('receipt', (receipt) => {
       setStartedGame(true);
   });
@@ -24,7 +32,14 @@ const Home = () => {
 
   return (
     <div>
-      {!startedGame?<button onClick={()=>startGame()}>Start Game</button>:<div>Game Started</div>}
+      {!startedGame?<button onClick={()=>startGame()}>Start Game</button>:
+      <div>
+        <div className='game__header'>
+          <h2>Game Started</h2>
+          <h2>Lives: 6</h2>
+        </div>
+      </div>
+      }
     </div>
   )
 }
